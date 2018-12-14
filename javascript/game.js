cb5fb0a21d0a929d5552582abb80a607afd539c7
Jason Kim <jasonkim4201@gmail.com>
2018-12-13 20:20:33 -0500

    added basic html and variables for javascript. corrected a few errors from misplaced semicolins and brackets

---
+++ javascript/game.js
@@ -0,0 +1,10 @@
+
+
+
+/* var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log
+for (i = 0; alphabet.length; i++) {
+
+};
+
+*/
